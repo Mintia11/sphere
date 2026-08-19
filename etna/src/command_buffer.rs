55016a9bb955466d60bf5efd7f60f08b46220c93
Franco Longo <129310461+Mintia11@users.noreply.github.com>
2026-08-19 16:55:19 +0200

    etna: split commands

--- etna/src/command_buffer.rs
+++ etna/src/command_buffer.rs
@@ -7,7 +7,7 @@ use crate::{Device, Image, error::Error};
 pub struct CommandBuffer {
     handle: vk::CommandBuffer,
 
-    device: Arc<Device>,
+    pub(crate) device: Arc<Device>,
 }
 
 impl Device {
@@ -57,34 +57,6 @@ impl CommandBuffer {
                 .handle()
                 .end_command_buffer(self.handle)
                 .map_err(Into::into)
-        }
-    }
-
-    pub fn image_pipeline_barrier(
-        &self,
-        image: &Image,
-        old_layout: vk::ImageLayout,
-        new_layout: vk::ImageLayout,
-    ) {
-        let image_memory_barrier = vk::ImageMemoryBarrier2::default()
-            .image(image.handle())
-            .new_layout(new_layout)
-            .old_layout(old_layout)
-            .subresource_range(
-                vk::ImageSubresourceRange::default()
-                    .aspect_mask(vk::ImageAspectFlags::COLOR)
-                    .base_array_layer(0)
-                    .base_mip_level(0)
-                    .layer_count(1)
-                    .level_count(1),
-            );
-
-        unsafe {
-            self.device.handle().cmd_pipeline_barrier2(
-                self.handle,
-                &vk::DependencyInfo::default()
-                    .image_memory_barriers(std::slice::from_ref(&image_memory_barrier)),
-            )
         }
     }
 }
--- etna/src/image.rs
+++ etna/src/image.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use ash::vk;
 use gpu_allocator::vulkan::Allocation;
 
-use crate::device::Device;
+use crate::{command_buffer::CommandBuffer, device::Device};
 
 pub struct Image {
     image: vk::Image,
@@ -30,6 +30,36 @@ impl Image {
 
     pub fn handle(&self) -> vk::Image {
         self.image
+    }
+}
+
+impl CommandBuffer {
+    pub fn image_pipeline_barrier(
+        &self,
+        image: &Image,
+        old_layout: vk::ImageLayout,
+        new_layout: vk::ImageLayout,
+    ) {
+        let image_memory_barrier = vk::ImageMemoryBarrier2::default()
+            .image(image.handle())
+            .new_layout(new_layout)
+            .old_layout(old_layout)
+            .subresource_range(
+                vk::ImageSubresourceRange::default()
+                    .aspect_mask(vk::ImageAspectFlags::COLOR)
+                    .base_array_layer(0)
+                    .base_mip_level(0)
+                    .layer_count(1)
+                    .level_count(1),
+            );
+
+        unsafe {
+            self.device.handle().cmd_pipeline_barrier2(
+                self.handle(),
+                &vk::DependencyInfo::default()
+                    .image_memory_barriers(std::slice::from_ref(&image_memory_barrier)),
+            )
+        }
     }
 }
 
