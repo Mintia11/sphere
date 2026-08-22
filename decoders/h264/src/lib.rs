bfc66ffe4417b36b3558f610ba960877b47babfa
Franco Longo <129310461+Mintia11@users.noreply.github.com>
2026-08-22 19:36:33 +0200

    h264: create a video decode session

--- decoders/h264/src/lib.rs
+++ decoders/h264/src/lib.rs
@@ -1,11 +1,12 @@
-use std::sync::Arc;
+use std::{marker::PhantomData, sync::Arc};
 
 use common::{
-    packet::{Error, PacketDecoder},
+    packet::{Error, Packet, PacketDecoder},
     track::TrackInfo,
 };
 use etna::{
-    Device,
+    Device, Image,
+    video::session::VideoSession,
     vk::{self, TaggedStructure},
 };
 
@@ -17,6 +18,8 @@ mod nal;
 pub struct H264Decoder {
     device: Arc<Device>,
     config: Option<Avcc>,
+
+    session: Option<VideoSession>,
 }
 
 impl H264Decoder {
@@ -24,7 +27,68 @@ impl H264Decoder {
         Self {
             device: device.clone(),
             config: None,
+
+            session: None,
         }
+    }
+
+    fn h264_profile<'a>(
+        config: &Avcc,
+        profile_h264: &'a mut vk::VideoDecodeH264ProfileInfoKHR<'a>,
+    ) -> vk::VideoProfileInfoKHR<'a> {
+        *profile_h264 = vk::VideoDecodeH264ProfileInfoKHR::default()
+            .std_profile_idc(config.profile.into())
+            .picture_layout(vk::VideoDecodeH264PictureLayoutFlagsKHR::PROGRESSIVE);
+
+        vk::VideoProfileInfoKHR::default()
+            .chroma_bit_depth(config.bit_depth_chroma())
+            .chroma_subsampling(config.chroma_format.into())
+            .luma_bit_depth(config.bit_depth_luma())
+            .video_codec_operation(vk::VideoCodecOperationFlagsKHR::DECODE_H264)
+            .push(profile_h264)
+    }
+
+    fn get_capabilities(
+        &self,
+        profile: &vk::VideoProfileInfoKHR,
+    ) -> Result<
+        (
+            vk::VideoCapabilitiesKHR<'_>,
+            vk::VideoDecodeCapabilitiesKHR<'_>,
+            vk::VideoDecodeH264CapabilitiesKHR<'_>,
+        ),
+        Error,
+    > {
+        let mut h264_capabilities = vk::VideoDecodeH264CapabilitiesKHR::default();
+        let mut decode_capabilities = vk::VideoDecodeCapabilitiesKHR::default();
+        let mut capabilities = vk::VideoCapabilitiesKHR::default()
+            .push(&mut decode_capabilities)
+            .push(&mut h264_capabilities);
+
+        unsafe {
+            self.device
+                .video_queue_instance_ext()
+                .get_physical_device_video_capabilities(
+                    self.device.physical_device(),
+                    profile,
+                    &mut capabilities,
+                )?;
+        }
+
+        Ok((
+            vk::VideoCapabilitiesKHR {
+                _marker: PhantomData,
+                ..capabilities
+            },
+            vk::VideoDecodeCapabilitiesKHR {
+                _marker: PhantomData,
+                ..decode_capabilities
+            },
+            vk::VideoDecodeH264CapabilitiesKHR {
+                _marker: PhantomData,
+                ..h264_capabilities
+            },
+        ))
     }
 }
 
@@ -57,41 +121,49 @@ impl PacketDecoder for H264Decoder {
     }
 
     fn can_decode_track(&self) -> Result<bool, Error> {
-        if let Some(config) = &self.config {
-            let mut profile_h264 = vk::VideoDecodeH264ProfileInfoKHR::default()
-                .std_profile_idc(config.profile.into())
-                .picture_layout(vk::VideoDecodeH264PictureLayoutFlagsKHR::PROGRESSIVE);
+        let Some(config) = &self.config else {
+            return Ok(false);
+        };
 
-            let profile = vk::VideoProfileInfoKHR::default()
-                .chroma_bit_depth(config.bit_depth_chroma())
-                .chroma_subsampling(config.chroma_format.into())
-                .luma_bit_depth(config.bit_depth_luma())
-                .video_codec_operation(vk::VideoCodecOperationFlagsKHR::DECODE_H264)
-                .push(&mut profile_h264);
+        let mut profile_h264 = vk::VideoDecodeH264ProfileInfoKHR::default();
+        let profile = Self::h264_profile(config, &mut profile_h264);
 
-            let mut h264_capabilities = vk::VideoDecodeH264CapabilitiesKHR::default();
-            let mut decode_capabilities = vk::VideoDecodeCapabilitiesKHR::default();
-            let mut capabilities = vk::VideoCapabilitiesKHR::default()
-                .push(&mut decode_capabilities)
-                .push(&mut h264_capabilities);
-
-            unsafe {
-                self.device
-                    .video_queue_instance_ext()
-                    .get_physical_device_video_capabilities(
-                        self.device.physical_device(),
-                        &profile,
-                        &mut capabilities,
-                    )?;
+        match self.get_capabilities(&profile) {
+            Ok(_) => Ok(true),
+            Err(e) => {
+                eprintln!("Video capability query failed: {e}");
+                Ok(false)
             }
-
-            println!("{capabilities:#?}");
-            println!("{decode_capabilities:#?}");
-            println!("{h264_capabilities:#?}");
-
-            Ok(true)
-        } else {
-            Ok(false)
         }
+    }
+
+    fn start_decode_session(&mut self) -> Result<(), Error> {
+        let Some(config) = &self.config else {
+            return Ok(());
+        };
+
+        let mut profile_h264 = vk::VideoDecodeH264ProfileInfoKHR::default();
+        let profile = Self::h264_profile(config, &mut profile_h264);
+        let (caps, _, _) = self.get_capabilities(&profile)?;
+
+        let formats = self
+            .device
+            .get_formats_for_profile(&profile, vk::ImageUsageFlags::VIDEO_DECODE_DST_KHR)?;
+
+        println!("Formats: {formats:#x?}");
+
+        let session = self.device.create_video_session(
+            &caps,
+            &profile,
+            formats[0].format,
+            formats[0].format,
+        )?;
+        self.session = Some(session);
+
+        Ok(())
+    }
+
+    fn send_packet(&mut self, packet: Packet) -> Result<(), Error> {
+        Ok(())
     }
 }
