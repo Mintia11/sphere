4e236629fc1e6dbb2ca0a069ab9c76e083527def
Franco Longo <129310461+Mintia11@users.noreply.github.com>
2026-08-23 17:14:12 +0200

    aac: parse the codec private data

--- decoders/aac/src/lib.rs
+++ decoders/aac/src/lib.rs
@@ -3,15 +3,39 @@ use common::{
     track::TrackInfo,
 };
 
-pub struct AacDecoder {}
+use crate::config::Config;
+
+mod config;
+
+#[derive(Default)]
+pub struct AacDecoder {
+    config: Option<Config>,
+}
 
 impl PacketDecoder for AacDecoder {
     fn track(&mut self, track: &TrackInfo) -> Result<(), Error> {
+        let private_data = track.extra_data.as_ref().ok_or(Error::InvalidData(
+            "Track has no codec private data".to_string(),
+        ))?;
+
+        self.config = Some(Config::parse(private_data)?);
+
         Ok(())
     }
 
     fn info_strings(&self) -> Vec<String> {
-        vec![]
+        let mut info_strings = Vec::new();
+
+        if let Some(config) = self.config {
+            info_strings.push(format!("Object Type: {:?}", config.object_type));
+            info_strings.push(format!("Sample Rate: {} Hz", config.sampling_frequency));
+            info_strings.push(format!(
+                "Channel Configuration: {}",
+                config.channel_configuration
+            ));
+        }
+
+        info_strings
     }
 
     fn can_decode_track(&self) -> Result<bool, Error> {
