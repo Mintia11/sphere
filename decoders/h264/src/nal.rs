463926ca6f8e4ec9e205012b10124db6ee8b8922
Franco Longo <129310461+Mintia11@users.noreply.github.com>
2026-08-25 19:07:26 +0200

    h264: parse lenght prefixed nals

--- decoders/h264/src/nal.rs
+++ decoders/h264/src/nal.rs
@@ -1,10 +1,11 @@
-use bytes::Bytes;
+use bytes::{Buf, Bytes};
 use common::{bit_io::BitReader, byte_io::ByteReader, packet::Error};
 use derive_try_from_primitive::TryFromPrimitive;
 
 #[derive(Debug)]
 pub struct RawNal {
     typ: NalType,
+    raw_hdr: u8,
     data: Bytes,
 }
 
@@ -28,6 +29,7 @@ impl RawNal {
 
         Ok(RawNal {
             typ,
+            raw_hdr: bytes[0],
             data: bytes.slice(1..),
         })
     }
@@ -64,4 +66,44 @@ impl RawNal {
 pub enum NalType {
     Sps = 0x7,
     Pps = 0x8,
+}
+
+pub struct LenghtPrefixedNal {
+    inner: RawNal,
+}
+
+impl LenghtPrefixedNal {
+    pub fn parse(mut bytes: Bytes, length_size: usize) -> Result<Vec<LenghtPrefixedNal>, Error> {
+        let mut nals = Vec::new();
+
+        while !bytes.is_empty() {
+            if bytes.len() < length_size {
+                return Err(Error::InvalidData("Truncated length prefix".into()));
+            }
+
+            let mut length = 0;
+            for &byte in &bytes[..length_size] {
+                length = (length << 8) | (byte as usize);
+            }
+
+            bytes.advance(length_size);
+
+            if bytes.len() < length {
+                return Err(Error::InvalidData("Truncated NAL payload".into()));
+            }
+
+            let nal_bytes = bytes.split_to(length);
+            let nal = RawNal::parse(&nal_bytes)?;
+            nals.push(LenghtPrefixedNal { inner: nal });
+        }
+
+        Ok(nals)
+    }
+
+    pub fn into_annex_b(self) -> Vec<u8> {
+        let mut data = Vec::with_capacity(4 + 1 + self.inner.data.len());
+        data.extend_from_slice(&[0x00, 0x00, 0x00, 0x01, self.inner.raw_hdr]);
+        data.extend_from_slice(&self.inner.data);
+        data
+    }
 }
