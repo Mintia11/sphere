bb0c62bcf0ccadc91ba97307157d47f01ca0b3a2
Franco Longo <129310461+Mintia11@users.noreply.github.com>
2026-08-26 17:59:03 +0200

    etna: Implement Vulkan Instance creation

--- etna/src/lib.rs
+++ etna/src/lib.rs
@@ -1,2 +1,6 @@
+#![allow(clippy::collapsible_if)]
+
 pub use ash::{self, vk};
 pub use gpu_allocator;
+
+pub mod instance;
--- sphere/src/main.rs
+++ sphere/src/main.rs
@@ -1,6 +1,14 @@
+use etna::instance::InstanceCreateInfo;
+use log::LevelFilter;
 use winit::event_loop::{ControlFlow, EventLoop};
 
 fn main() {
+    pretty_env_logger::formatted_builder()
+        .filter_level(LevelFilter::Trace)
+        .init();
+
     let event_loop = EventLoop::new().unwrap();
     event_loop.set_control_flow(ControlFlow::Poll);
+
+    let instance = etna::instance::Instance::new(InstanceCreateInfo { debug: true });
 }
