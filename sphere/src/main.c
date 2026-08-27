6b648eaa47b96e35dce148bad64e531154955d1a
Franco Longo <129310461+Mintia11@users.noreply.github.com>
2026-08-27 20:12:54 +0200

    misc: test my vector impl

--- sphere/src/main.c
+++ sphere/src/main.c
@@ -1,10 +1,26 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <mutex.h>
 #include <alloc.h>
+#include <Volk/volk.h>
+#include "vec.h"
 
 int main() {
     etna_allocator_init_global();
 
     printf("Hello world!\n");
-    void* mem = ETNA_ALLOC_TYPE(main, int);
+    void* mem = ETNA_ALLOC_TYPE(NULL, int);
+    ETNA_FREE(mem);
+
+    ETNA_VEC(uint32_t) ciao = ETNA_VEC_INIT;
+    ETNA_VEC_PUSH(&ciao, 1);
+    ETNA_VEC_PUSH(&ciao, 2);
+    ETNA_VEC_PUSH(&ciao, 3);
+    ETNA_VEC_PUSH(&ciao, 4);
+    ETNA_VEC_PUSH(&ciao, 5);
+
+    ETNA_VEC_FOR_EACH_ENTRY(&ciao, idx) {
+        printf("%d\n", ETNA_VEC_AT(&ciao, idx));
+        ETNA_VEC_PUSH(&ciao, 5);
+    }
 }
