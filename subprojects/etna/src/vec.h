df67b549e20c231e9ef729d21e382fa21404f997
Franco Longo <129310461+Mintia11@users.noreply.github.com>
2026-08-27 20:22:02 +0200

    etna: impl vec resize

--- subprojects/etna/src/vec.h
+++ subprojects/etna/src/vec.h
@@ -13,17 +13,22 @@
 
 #define ETNA_VEC_AT(vec, idx) (vec)->data[idx]
 
-#define ETNA_VEC_PUSH(vec, value)                                                              \
-    do {                                                                                       \
+#define ETNA_VEC_RESIZE(vec, new_size)                                                         \
+    if ((vec)->capacity != new_size) {                                                         \
         if ((vec)->capacity == 0) {                                                            \
-            (vec)->capacity = 4;                                                               \
+            (vec)->capacity = new_size;                                                        \
             (vec)->data = ETNA_ALLOC(NULL, (vec)->capacity * sizeof(*((vec)->data)));          \
         } else if ((vec)->length == (vec)->capacity) {                                         \
-            (vec)->capacity = (vec)->capacity * 2;                                             \
+            (vec)->capacity = new_size;                                                        \
             (vec)->data = ETNA_REALLOC((vec)->data, (vec)->capacity * sizeof(*((vec)->data))); \
         }                                                                                      \
-        (vec)->data[(vec)->length] = value;                                                    \
-        (vec)->length += 1;                                                                    \
+    }
+
+#define ETNA_VEC_PUSH(vec, value)                                              \
+    do {                                                                       \
+        ETNA_VEC_RESIZE((vec), (vec)->capacity != 0 ? (vec)->capacity * 2 : 4) \
+        (vec)->data[(vec)->length] = value;                                    \
+        (vec)->length += 1;                                                    \
     } while (0);
 
 #define ETNA_VEC_POP(vec, out_val)                   \
