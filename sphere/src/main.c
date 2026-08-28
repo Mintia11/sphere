63adca19c920b688f44089589fed02734eaba7e8
Franco Longo <129310461+Mintia11@users.noreply.github.com>
2026-08-28 13:27:13 +0200

    sphere: test logging

--- sphere/src/main.c
+++ sphere/src/main.c
@@ -1,14 +1,17 @@
 #include <stdint.h>
-#include <stdio.h>
 #include <mutex.h>
 #include <alloc.h>
 #include <Volk/volk.h>
+#include "log.h"
 #include "vec.h"
 
 int main() {
     etna_allocator_init_global();
+    etna_logger_init_global();
 
-    printf("Hello world!\n");
+    etna_log_scope_t* main_log = etna_log_scope_new("main", NULL);
+
+    ETNA_INFO(main_log, "Hello world!\n");
     void* mem = ETNA_ALLOC_TYPE(NULL, int);
     ETNA_FREE(mem);
 
@@ -19,8 +22,11 @@ int main() {
     ETNA_VEC_PUSH(&ciao, 4);
     ETNA_VEC_PUSH(&ciao, 5);
 
+    etna_log_scope_t* loop_log = etna_log_scope_new("loop", main_log);
+
     ETNA_VEC_FOR_EACH_ENTRY(&ciao, idx) {
-        printf("%d\n", ETNA_VEC_AT(&ciao, idx));
-        ETNA_VEC_PUSH(&ciao, 5);
+        ETNA_INFO(loop_log, "%d\n", ETNA_VEC_AT(&ciao, idx));
     }
+
+    ETNA_FREE(main);
 }
